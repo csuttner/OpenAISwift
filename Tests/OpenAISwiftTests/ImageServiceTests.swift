dd0e97821eb01084e39e7986ebc0fb086e58473b
Clay Suttner <csuttner@gmail.com>
2024-03-16 19:55:53 -0700

    Image edit tests

--- Tests/OpenAISwiftTests/ImageServiceTests.swift
+++ Tests/OpenAISwiftTests/ImageServiceTests.swift
@@ -21,24 +21,50 @@ final class ImageServiceTests: XCTestCase {
 
     func test_fetchImageGeneration() async throws {
         let service = ImageService()
-        let payload = ImageGenerationPayload(prompt: "an image of a surfer")
+
+        let payload = ImageGenerationPayload(
+            prompt: "an image of a surfer"
+        )
         
-        let response = try await service.fetchImageGeneration(payload: payload)
+        let response = try await service
+            .fetchImageGeneration(payload: payload)
         
         XCTAssertNotNil(response.data.first?.url)
     }
     
     func test_fetchImageEdit() async throws {
+        let service = ImageService()
         
+        let payload = ImageEditPayload(
+            image: imageData("Wave"),
+            prompt: "a surfer on the wave",
+            mask: imageData("WaveMask")
+        )
+        
+        let response = try await service
+            .fetchImageEdit(payload: payload)
+        
+        XCTAssertNotNil(response.data.first?.url)
     }
     
     func test_fetchImageVariation() async throws {
         let service = ImageService()
-        let image = UIImage(named: "Wave", in: Bundle.module, with: .none)!
-        let payload = ImageVariationPayload(image: image.pngData()!)
         
-        let response = try await service.fetchImageVariation(payload: payload)
+        let payload = ImageVariationPayload(
+            image: imageData("Wave")
+        )
+        
+        let response = try await service
+            .fetchImageVariation(payload: payload)
         
         XCTAssertNotNil(response.data.first?.url)
+    }
+    
+    private func imageData(_ name: String) -> Data {
+        UIImage(
+            named: name,
+            in: Bundle.module,
+            with: .none
+        )!.pngData()!
     }
 }
